43b8218511608027b05fa9180290917c2a849ca5
Bill Ticehurst <billti@hotmail.com>
2015-02-18 00:04:48 -0800

    Got a document parsing in the TypeScript LS

--- public/app/typescriptMode.js
+++ public/app/typescriptMode.js
@@ -5,6 +5,7 @@ CodeMirror.defineMode("typescript", function(config, modeConfig) {
 		},
 		token: function(stream, state) {
 			// Return a style string.  Use the standard style theme names (without 'cm-' prefix)
+			// See codemirror.css @ 95 for the list of classes.
 			var result = null;
 			while(!stream.eol()){
 				var nextChar = stream.next();
@@ -28,3 +29,40 @@ CodeMirror.registerHelper("hint", "typescript", function(editor, options){
             from: CodeMirror.Pos(cur.line, token.start),
             to: CodeMirror.Pos(cur.line, token.end)};
 });
+
+// Minimal hack for now to understand the LS API plumbing basics
+var tsls = {};
+tsls.compilerOptions = ts.getDefaultCompilerOptions();
+tsls.docName = "sample.ts";
+tsls.docText = "class Foo { public age = 42 }";
+tsls.docVersion = "1";
+tsls.IScriptSnapshot = {
+	getText: function(start, end) { return tsls.docText.substring(start, end); },
+	getLength: function() { return tsls.docText.length; },
+	getChangeRange: function() { return undefined; }
+};
+tsls.host = {
+	    getCompilationSettings: function (){ return tsls.compilerOptions; },
+        getNewLine: function (){ return "\n"; },
+        getScriptFileNames: function (){ return [tsls.docName]; },
+        getScriptVersion: function (fileName) { return tsls.docVersion; },
+        getScriptSnapshot: function (fileName) { return tsls.IScriptSnapshot; },
+        getCurrentDirectory: function() { return "/"; },
+        getDefaultLibFileName: function (options) { return "lib.d.ts"; },
+        //getLocalizedDiagnosticMessages?(): any;
+        //getCancellationToken?(): CancellationToken;
+        //log?(s: string): void;
+        //trace?(s: string): void;
+        //error?(s: string): void;
+};
+tsls.docRegistry = ts.createDocumentRegistry();
+tsls.service = ts.createLanguageService(tsls.host, tsls.docRegistry);
+tsls.docRegistry.acquireDocument(tsls.docName, tsls.compilerOptions, tsls.IScriptSnapshot, tsls.docVersion);
+
+tsls.classifications = tsls.service.getSyntacticClassifications("sample.ts", {start: 0, length: tsls.IScriptSnapshot.getLength() });
+
+/* TODO
+1. Wire up the editor change events to update the document.
+2. Wire up the typescript mode token function to use the syntactic classifications.
+3. Encapsulate all this (and the script in index.html) nicely.
+*/
